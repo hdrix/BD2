ae281023a0898e0df4e6aad744c106cba1786d5b
abnner12 <abnner_1995@hotmail.com>
2017-05-11 09:29:44 -0600

    Palma

--- BD2/CL/DBInitializer.cs
+++ BD2/CL/DBInitializer.cs
@@ -45,6 +45,54 @@ namespace BD2.CL
                 context.Empleados.Add(e);
             }
             context.SaveChanges();
+
+
+            //table Encomienda, Passwords and TipoEstado
+
+            var encomienda = new Encomienda[]
+            {
+                new Encomienda{ID=1,Nombre="N1", Descrip="Descrip 1"},
+                new Encomienda{ID=2,Nombre="N2", Descrip="Descrip 2"},
+                new Encomienda{ID=3,Nombre="N3", Descrip="Descrip 3"},
+                new Encomienda{ID=4,Nombre="N4", Descrip="Descrip 4"},
+                new Encomienda{ID=5,Nombre="N5", Descrip="Descrip 5"},
+            };
+
+            foreach (Encomienda enc in encomienda)
+            {
+                context.Encomienda.Add(enc);
+            }
+            context.SaveChanges();
+
+            var passwords = new Passwords[]
+            {
+                new Passwords{ID=1,Fecha=new DateTime(2013, 12, 01), Secret="Secret 1"},
+                new Passwords{ID=2,Fecha=new DateTime(2017, 11, 07), Secret="Secret 2"},
+                new Passwords{ID=3,Fecha=new DateTime(2016, 09, 14), Secret="Secret 3"},
+                new Passwords{ID=4,Fecha=new DateTime(2015, 08, 05), Secret="Secret 4"},
+                new Passwords{ID=5,Fecha=new DateTime(2014, 10, 23), Secret="Secret 5"},
+            };
+
+            foreach (Passwords pass in passwords)
+            {
+                context.Passwords.Add(pass);
+            }
+            context.SaveChanges();
+
+            var tipoEstado = new TipoEstado[]
+           {
+                new TipoEstado{ID=1,Nombre="N1", Descrip="Descrip 1"},
+                new TipoEstado{ID=2,Nombre="N2", Descrip="Descrip 2"},
+                new TipoEstado{ID=3,Nombre="N3", Descrip="Descrip 3"},
+                new TipoEstado{ID=4,Nombre="N4", Descrip="Descrip 4"},
+                new TipoEstado{ID=5,Nombre="N5", Descrip="Descrip 5"},
+           };
+
+            foreach (TipoEstado ties in tipoEstado)
+            {
+                context.TipoEstado.Add(ties);
+            }
+            context.SaveChanges();
         }
 
         
