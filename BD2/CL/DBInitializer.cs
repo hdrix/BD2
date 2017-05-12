90c639c0e7c21759f67d67721a4d6797f4e5fdc7
jalvarez08 <jalvarez0896@gmail.com>
2017-05-11 22:06:46 -0600

    Cambios Palma Agregados

--- BD2/CL/DBInitializer.cs
+++ BD2/CL/DBInitializer.cs
@@ -45,6 +45,52 @@ namespace BD2.CL
                 context.Empleados.Add(e);
             }
             context.SaveChanges();
+
+            var encomienda = new Encomienda[]
+            {
+                new Encomienda{Nombre="N1", Descrip="Descrip 1"},
+                new Encomienda{Nombre="N2", Descrip="Descrip 2"},
+                new Encomienda{Nombre="N3", Descrip="Descrip 3"},
+                new Encomienda{Nombre="N4", Descrip="Descrip 4"},
+                new Encomienda{Nombre="N5", Descrip="Descrip 5"},
+            };
+
+            foreach (Encomienda enc in encomienda)
+            {
+                context.Encomienda.Add(enc);
+            }
+            context.SaveChanges();
+
+
+            var passwords = new Passwords[]
+            {
+                new Passwords{Fecha=new DateTime(2013, 12, 01), Secret="Secret 1"},
+                new Passwords{Fecha=new DateTime(2017, 11, 07), Secret="Secret 2"},
+                new Passwords{Fecha=new DateTime(2016, 09, 14), Secret="Secret 3"},
+                new Passwords{Fecha=new DateTime(2015, 08, 05), Secret="Secret 4"},
+                new Passwords{Fecha=new DateTime(2014, 10, 23), Secret="Secret 5"},
+            };
+
+            foreach (Passwords pass in passwords)
+            {
+                context.Passwords.Add(pass);
+            }
+            context.SaveChanges();
+
+            var tipoEstado = new TipoEstado[]
+            {
+                new TipoEstado{Nombre="N1", Descrip="Descrip 1"},
+                new TipoEstado{Nombre="N2", Descrip="Descrip 2"},
+                new TipoEstado{Nombre="N3", Descrip="Descrip 3"},
+                new TipoEstado{Nombre="N4", Descrip="Descrip 4"},
+                new TipoEstado{Nombre="N5", Descrip="Descrip 5"},
+            };
+
+            foreach (TipoEstado ties in tipoEstado)
+            {
+                context.TipoEstado.Add(ties);
+            }
+            context.SaveChanges();
         }
 
         
